cb02d4e20d731f50b1fb0cfdcbc363e3ef2efd41
b <131214043+ItaloMaia17@users.noreply.github.com>
2026-09-03 12:01:00 -0300

    ajuste nos experimento e nos modelos

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ MODELS: dict[str, str] = {
     "laguna-s-2.1:free" : "poolside/laguna-s-2.1:free", # teste
     "llama-3.3-70b": "meta-llama/llama-3.3-70b-instruct:free",
     "ministral-14b-2512": "mistralai/ministral-14b-2512",
-    "gemma-4-31b": "google/gemma-4-31b-it:free",
+    "gemma-4-31b-it:free": "google/gemma-4-31b-it:free",
     "qwen3-next-80b-a3b-instruct": "qwen/qwen3-next-80b-a3b-instruct:free",
     "openai/gpt-oss-120b": "openai/gpt-oss-120b:free",
 }
